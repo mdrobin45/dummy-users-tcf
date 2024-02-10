e2398bbd91d7cf2def275266b364f6fcedd2c805
Md Robin Rana <developer.smrobin45@gmail.com>
2024-02-10 08:57:43 +0600

    search form added

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({ children }) => {
-   return <div className="max-w-[1200px] mx-auto p-4 md:p-0">{children}</div>;
+   return <div className="max-w-[1250px] mx-auto p-4 md:p-0">{children}</div>;
 };
 
 export default Container;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Container from "../../Components/Container/Container";
 import UserCard from "../../Components/UserCard/UserCard";
+import SearchForm from "./SearchForm/SearchForm";
 
 const Home = () => {
    const { isPending, data } = useQuery({
@@ -11,17 +12,20 @@ const Home = () => {
 
    return (
       <Container>
-         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-20">
-            {!isPending ? (
-               <>
-                  {data?.data?.users.map((item) => (
-                     <UserCard key={item.id} user={item} />
-                  ))}
-               </>
-            ) : (
-               <p>Loading</p>
-            )}
-         </div>
+         <section className="my-20">
+            <SearchForm />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+               {!isPending ? (
+                  <>
+                     {data?.data?.users.map((item) => (
+                        <UserCard key={item.id} user={item} />
+                     ))}
+                  </>
+               ) : (
+                  <p>Loading</p>
+               )}
+            </div>
+         </section>
       </Container>
    );
 };
